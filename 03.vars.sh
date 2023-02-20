2b6ecf017eb608c8c405fc635d04d0e88f2448b2
vlnaresh28 <vlnareshreddy@gmail.com>
2023-02-20 09:06:03 +0000

    c4

--- 03.vars.sh
+++ 03.vars.sh
@@ -3,7 +3,7 @@ DATE=2023-02-20
 echo " good morning today date is " \e  \e [35m$ {DATE} \e[0m
  echo " good morning today date is " -e \e[ 35m $ {DATE} \e[0m
 
- echo -e "\e[35m MESSAGE \e[0m"
+ echo -e "\e[35m good morning today date is ${DATE} \e[0m"
  echo  good morning today date is  ${DATE}  
  
  echo  good morning today date is   -e "\e[37m ${DATE} \e[0m"
